b55fd9930d8547b3e544af85ea2d3b932858cf84
Russell <Russell.J.Baker@gmail.com>
2014-12-13 10:56:37 -0500

    integrated arduino

--- Plugins/ArduinoPlugin/Source/ArduinoPlugin/Classes/ArduinoPluginActor.h
+++ Plugins/ArduinoPlugin/Source/ArduinoPlugin/Classes/ArduinoPluginActor.h
@@ -6,16 +6,28 @@
 UCLASS()
 class AArduinoPluginActor : public AActor
 {
-	GENERATED_UCLASS_BODY()
-
-	UFUNCTION(BlueprintCallable, Category="ArduinoFunctions")
-	void SetFanSpeed(uint8 fan1, uint8 fan2);
+    GENERATED_UCLASS_BODY()
 
     UFUNCTION(BlueprintCallable, Category = "ArduinoFunctions")
-    void ConnectArduino(FString comPort);
+    void SetFanSpeed(uint8 fan1, uint8 fan2);
 
     UFUNCTION(BlueprintCallable, Category = "ArduinoFunctions")
-    void Disconnect();
+        void ConnectArduino();
+
+    UFUNCTION(BlueprintCallable, Category = "ArduinoFunctions")
+        void Disconnect();
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "ArduinoFunction")
+        void FanSpeedAdjusted(uint8 speed1, uint8 speed2);
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Arduino)
+        uint8 speed1;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Arduino)
+        uint8 speed2;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Arduino)
+        FString comPort;
 };
 
 
